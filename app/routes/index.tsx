d08fe629ff79499aa7d03e146066cb823dfbe07f
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-03-16 22:06:51 +0200

    clean up index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,7 @@
-import Event from "~/components/Event";
-
 export default function Index() {
-  const events: FresherEvent[] = [
-    { id: 'test', title: 'Test Event' },
-    { id: 'other', title: 'Another test event!' }
-  ]
-
   return (
     <div>
       <h1>Fresher Pass</h1>
-      <h2>Events</h2>
-      {events.map(event => <Event key={event.id} event={event} />)}
     </div>
   );
 }
