812ccab5e5fde5559630f1dd84d4b3d937153b48
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-03-16 21:38:24 +0200

    add prisma
    Prisma has been added with SQLite. In the future it will be migrated to PostgreSQL.

--- README.md
+++ README.md
@@ -6,6 +6,39 @@ Aims to be usable on mobile through "Add to home screen" or similar functionalit
 
 Current features to be implemented can be found in [TODO](https://github.com/ConcernedHobbit/fresher-pass/blob/main/docs/TODO.md).
 
+# Installation
+
+1. Clone project locally
+```bash
+  git clone https://github.com/ConcernedHobbit/fresher-pass.git
+  cd fresher-pass
+```
+
+2. Install dependencies
+```bash
+  npm install
+```
+
+3. Initialize the database
+```bash
+  npx prisma migrate dev --name initialization
+```
+
+## Developing
+Start the development server:
+```bash
+  npm run dev
+```
+It will automatically update when it detects changes to the source.
+
+## Building & deploying
+Building and running the application in production mode is simple:
+```bash
+  npm run build
+  npm start
+```
+**At this point of the project it is not recommended to run in production mode.**
+
 ## Full Stack Open
 
 This is a course project for the [Full Stack Open](https://fullstackopen.com/en) course.
---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const events: Prisma.EventCreateInput[] = [
+  { title: "Fresher year head start" },
+  { title: "KJYR 2027" },
+  { title: "Fresher sitz" },
+  { title: "Unisport Reservation" }
+]
+
+const users: Prisma.UserCreateInput[] = [
+  { username: "chobbit", name: "Eetu Raunio" },
+  { username: "teoteppana", name: "Teo Uosukainen" },
+  { username: "fresher", name: "Alfred J. Kwak" }
+]
+
+async function seed() {
+  users.forEach(async (seedUser) => {
+    const user = await prisma.user.create({
+      data: seedUser
+    });
+
+    console.log(`Seeded user '${user.username}' (id ${user.id})`)
+  });
+
+  events.forEach(async (seedEvent) => {
+    const event = await prisma.event.create({
+      data: seedEvent
+    });
+
+    console.log(`Seeded event '${event.title}' (id ${event.id})`)
+  })
+}
+
+// Wish we had top-level await...
+// Promises will have to do.
+seed()
+  .then(() => console.log(`Finished seeding. Exiting...`))
+  .catch((e: unknown) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(() => prisma.$disconnect);
