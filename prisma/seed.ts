19b9014d258e73f1d621e181c954bf0e1be0b502
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-03-28 17:42:39 +0300

    update schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,16 +3,16 @@ import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const events: Prisma.PassEventCreateInput[] = [
-  { title: "Fresher year head start" },
-  { title: "KJYR 2027" },
-  { title: "Fresher sitz" },
-  { title: "Unisport Reservation" }
+  { title: "Fresher year head start", place: "Suomenlinna", start: new Date("2022-08-01") },
+  { title: "KJYR 2027", description: "Fresher cruise with Kumpula organizations", start: new Date("2022-11-05") },
+  { title: "Fresher sitz", description: "The first sitz! We'll teach you everything.", place: "Alina-sali", start: new Date("2022-09-09") },
+  { title: "Unisport Reservation", description: "Weekly sportings", place: "UniSport Kumpula" }
 ]
 
 const users: Prisma.UserCreateInput[] = [
   { username: "chobbit", name: "Eetu Raunio" },
   { username: "teoteppana", name: "Teo Uosukainen" },
-  { username: "fresher", name: "Alfred J. Kwak" }
+  { username: "fresher" }
 ]
 
 async function seed() {
