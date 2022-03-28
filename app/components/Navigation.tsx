4e4b8eadb5953c381dc4e81a6f73e1ced108fe13
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-03-28 17:34:40 +0300

    update navigation to match routes

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -3,7 +3,7 @@ export default function Navigation(): JSX.Element {
     <nav>
       <ul><a href="/">Fresher Pass</a></ul>
       <ul><a href="/events">Events</a></ul>
-      <ul><a href="/tutors">Tutors</a></ul>
+      <ul><a href="/users">Tutors</a></ul>
       <ul><a href="/me">Profile</a></ul>
       <ul><a href="/about">About</a></ul>
       <ul><a href="/dokaa">Fuck shit up</a></ul>
